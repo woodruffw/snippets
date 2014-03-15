31ad54e5b1bdca9246bf5ccfd461c957dd46a1d0
William Woodruff <william@tuffbizz.com>
2014-03-15 11:38:20 -0400

    added magnet-to-torrent

---
+++ magnet-to-torrent/magnet-to-torrent.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/env perl
+
+#  magnet-to-torrent.pl
+#  Author: William Woodruff
+#  ------------------------
+#  Converts the provided magnet link into a usable torrent file (with a semiunique name) using magnet2torrent.com.
+#  Note: magnet2torrent.com's owners may not appreciate bulk transactions through their website. Use sparingly, or ask first.
+#  Depends on WWW::Mechanize for form submission.
+#  ------------------------
+#  This code is licensed by William Woodruff under the MIT License.
+#  http://opensource.org/licenses/MIT
+
+use strict;
+use warnings;
+
+use WWW::Mechanize;
+
+my $magnet = shift || die "Usage: magnet-to-torrent.pl <\"magnet link\">\n";
+my $url = 'http://magnet2torrent.com/';
+my $mech = WWW::Mechanize->new();
+$mech->get($url);
+
+my $result = $mech->submit_form(form_number => 1, fields => { magnet => $magnet });
+$mech->save_content(time() . 'x' . rand() . '.torrent');
