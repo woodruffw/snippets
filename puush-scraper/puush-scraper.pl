5016c4e9d6f07c2a6f953deae5bc9978b1c95aa7
William Woodruff <william@tuffbizz.com>
2014-04-19 00:00:49 -0400

    added puush-scraper

---
+++ puush-scraper/puush-scraper.pl
@@ -0,0 +1,61 @@
+#!/usr/bin/env perl
+
+#  puush-scraper.pl
+#  Author: William Woodruff
+#  ------------------------
+#  Scrapes puu.sh, downloading n semirandom files via semirandom brute-guessing
+#  Stores downloaded files in a timestamped directory
+#  Depends upon WWW::Mechanize and HTTP::Response.
+#  ------------------------
+#  This code is licensed by William Woodruff under the MIT License.
+#  http://opensource.org/licenses/MIT
+
+use strict;
+use warnings;
+
+use WWW::Mechanize;
+use HTTP::Response;
+
+my @table = (0..9, 'a'..'z', 'A'..'Z');
+
+my $num_files = shift || die "Usage: puush-scraper.pl <number of files>\n";
+my $mech = WWW::Mechanize->new(agent => 'puush-scraper/perl/iamarobot', autocheck => 0);
+
+mkdir "scrape";
+
+my $file_count = 1;
+while ($file_count <= $num_files)
+{
+	my $item = $table[int(rand(62))] . $table[int(rand(62))] . $table[int(rand(62))] . $table[int(rand(62))] . $table[int(rand(62))];
+	my $url = "http://www.puu.sh/$item";
+	my $response = $mech->get($url);
+
+	if ($response->is_success)
+	{
+		print "FILE FOUND ($item): $file_count out of $num_files\n";
+		my $content = $mech->ct();
+
+		if ($content =~ /jpeg/)
+		{
+			$mech->save_content("scrape/$item.jpg", decoded_by_headers => 1);
+		}
+		elsif ($content =~ /png/)
+		{
+			$mech->save_content("scrape/$item.png", decoded_by_headers => 1);
+		}
+		elsif ($content =~ /gif/)
+		{
+			$mech->save_content("scrape/$item.gif", decoded_by_headers => 1);
+		}
+		elsif ($content =~ /text/)
+		{
+			$mech->save_content("scrape/$item.txt", decoded_by_headers => 1);
+		}
+		else
+		{
+			$mech->save_content("scrape/$item.other", decoded_by_headers => 1);
+		}
+
+		$file_count++;
+	}
+}
