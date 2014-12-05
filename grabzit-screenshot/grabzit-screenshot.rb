19a6a4eb0163604a7505ca5d8a53ddb423fdbb30
William Woodruff <william@tuffbizz.com>
2014-12-05 11:52:41 -0500

    add grabzit-screenshot

---
+++ grabzit-screenshot/grabzit-screenshot.rb
@@ -0,0 +1,12 @@
+require 'grabzit'
+
+unless ARGV.length == 2
+	puts "Usage: $0 <url> <file>\n"
+	exit 1
+end
+
+client = GrabzIt::Client.new(ENV['GRABZIT_API_KEY'], ENV['GRABZIT_API_SECRET'])
+url = ARGV.shift
+file = ARGV.shift
+client.set_image_options(url)
+client.save_to(file)
