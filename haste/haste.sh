784e0e2dd95ef44eb1cc5642adb212775f1ae9b6
William Woodruff <william@tuffbizz.com>
2015-02-19 12:55:58 -0500

    add haste.sh

---
+++ haste/haste.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# haste.sh - post files to hastebin and print a link. requires curl.
+# usage: haste.sh <file> or pipe into haste
+# ----
+# Author: William Woodruff
+# Licensed under the MIT License: http://opensource.org/licenses/MIT
+
+if [[ -z "${1}" || "${1}" =~ "-" ]]; then
+	data=$(cat)
+elif [[ -f "${1}" ]]; then
+	data=$(cat ${1})
+else
+	echo "Usage: $0 <file>"
+	exit 1
+fi
+
+response=$(curl -X POST -s -d "${data}" http://hastebin.com/documents)
+echo "${response}" | awk -F '"' '{print "http://hastebin.com/"$4}'
