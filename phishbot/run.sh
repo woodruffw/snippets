fd34eb0dd1280160c23662b451d568e3792192df
William Woodruff <william@tuffbizz.com>
2015-05-28 20:57:48 -0400

    style uniformity

--- phishbot/run.sh
+++ phishbot/run.sh
@@ -7,6 +7,6 @@ channel="#phishbot"
 nicks=(NickSerc NickSerf MickServ KickServ) # limit it to 4 for now
 
 for nick in "${nicks[@]}"; do
-	ruby ./phishbot.rb "$network" "$nick" "$channel" 2>/dev/null &
+	ruby ./phishbot.rb "${network}" "${nick}" "${channel}" 2>/dev/null &
 	echo "Kill this pid to end ${nick} on ${network}: $!"
 done
