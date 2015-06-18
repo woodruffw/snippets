ebebe71c5ed70b1bcc9f51251695b43b7e676a5a
William Woodruff <william@tuffbizz.com>
2015-06-18 11:39:50 -0400

    add socketdump and socketslurp

---
+++ socketdump/socketdump.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+
+#	socketdump.rb
+#	Author: William Woodruff
+#	------------------------
+#	Dumps a given file onto a connected socket, headed by a SHA1 hash and name.
+#	Useful for quickly shooting data between two machines.
+#	Complemented by socketslurp.rb
+#	------------------------
+#	This code is licensed by William Woodruff under the MIT License.
+#	http://opensource.org/licenses/MIT
+
+require 'socket'
+require 'digest/sha1'
+require 'base64'
+
+if ARGV.size != 2
+	abort("Usage: #{$0}: <port> <file>")
+end
+
+begin
+	port = Integer(ARGV[0])
+	data = Base64.encode64(open(ARGV[1]).read)
+	hash = Digest::SHA1.hexdigest(data)
+rescue Exception => e
+	abort("Fatal: #{e.to_s}")
+end
+
+server = TCPServer.new(port)
+
+loop do
+	Thread.start(server.accept) do |client|
+		client.puts("#{hash} #{File.basename(ARGV[1])}")
+		client.puts(data)
+		client.close
+	end
+end
---
+++ socketslurp/socketslurp.rb
@@ -0,0 +1,35 @@
+#!/usr/bin/env ruby
+
+#	socketslurp.rb
+#	Author: William Woodruff
+#	------------------------
+#	Reads a file from a socket, validating it using a SHA1 hash and saving it.
+#	Useful for quickly shooting data between two machines.
+#	Complemented by socketdump.rb
+#	------------------------
+#	This code is licensed by William Woodruff under the MIT License.
+#	http://opensource.org/licenses/MIT
+
+require 'socket'
+require 'digest/sha1'
+require 'base64'
+
+if ARGV.size != 2
+	abort("Usage: #{$0} <hostname> <port>")
+end
+
+begin
+	sock = TCPSocket.new(ARGV[0], Integer(ARGV[1]))
+	hash, filename = sock.gets.split(' ')
+	data = sock.read
+rescue Exception => e
+	abort("Fatal: #{e.to_s}")
+end
+
+if hash != Digest::SHA1.hexdigest(data)
+	abort("Fatal: hash mismatch for received file.")
+else
+	File.write(filename, Base64.decode64(data))
+end
+
+sock.close
