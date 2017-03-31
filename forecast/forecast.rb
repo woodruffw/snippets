4370d86206990c6047a9eacba0a0f12ccaac99a3
William Woodruff <william@tuffbizz.com>
2017-03-31 13:42:49 -0400

    Add forecast.
    Fetches the weather forecast from Wunderground.

---
+++ forecast/forecast.rb
@@ -0,0 +1,35 @@
+#!/usr/bin/env ruby
+
+# forecast.rb
+# Author: William Woodruff
+# ------------------------
+# Fetches a weather forecast for a given query from Wunderground.
+# Requires the "wunderground" gem.
+# ------------------------
+# This code is licensed by William Woodruff under the MIT License.
+# http://opensource.org/licenses/MIT
+
+begin
+  require "wunderground"
+rescue LoadError
+  abort "I need the 'wunderground' gem."
+end
+
+KEY = ENV["WUNDERGROUND_API_KEY"]
+
+abort("I need a $WUNDERGROUND_API_KEY in your environment.") unless KEY
+
+wunder = Wunderground.new(KEY)
+
+begin
+  forecast = wunder.forecast_for(ARGV.join(" "))["forecast"]
+
+  # even period numbers are day forecasts, odds are night forecasts
+  days = forecast["txt_forecast"]["forecastday"].select { |w| w["period"].even? }
+rescue => e
+  puts "Something blew up: #{e.to_s}."
+end
+
+days.each do |day|
+  puts "#{day["title"]} - #{day["fcttext"]}"
+end
