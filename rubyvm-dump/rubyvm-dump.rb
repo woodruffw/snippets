ac6248e30e31b2cd3040eda28c8207ce05d710f8
William Woodruff <william@tuffbizz.com>
2017-04-04 15:57:39 -0400

    Add rubyvm-dump.
    Dumps a Ruby script to YARV instructions, which can then be saved
    either in plain text or in a Marshal-serialized blob.

---
+++ rubyvm-dump/rubyvm-dump.rb
@@ -0,0 +1,50 @@
+#!/usr/bin/env ruby
+
+require "slop"
+
+# rubyvm-dump.rb
+# Author: William Woodruff
+# ------------------------
+# Dump a Ruby script as a compiled sequence of instructions.
+# Requires the "slop" gem.
+# ------------------------
+# This code is licensed by William Woodruff under the MIT License.
+# http://opensource.org/licenses/MIT
+
+VERSION = 1
+
+opts = Slop.parse do |o|
+  o.banner = <<~EOS
+    Dump a Ruby script as a compiled sequence of instructions.
+    Saves the result with a .vm suffix.
+
+    Usage:
+      rubyvm-dump [--marshal] <file>
+  EOS
+
+  o.bool "-m", "--marshal", "Serialize the instructions (not human readable)"
+
+  o.on "-V", "--version", "print the script's version" do
+    puts "rubyvm-dump version #{VERSION}."
+    exit
+  end
+
+  o.on "-h", "--help", "print this help message" do
+    puts o
+    exit
+  end
+end
+
+input = opts.args.shift || abort("I need a file to dump.")
+output = "#{input}.vm"
+source = File.read(input)
+
+instructions = RubyVM::InstructionSequence.new source
+
+if opts.marshal?
+  contents = Marshal.dump(instructions.to_a)
+else
+  contents = instructions.disasm
+end
+
+File.write(output, contents)
