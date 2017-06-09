8ed94c9ff71c0e0f18f6725c58fb3c4bc0a15339
William Woodruff <william@tuffbizz.com>
2017-06-09 18:38:32 -0400

    ssqdb: Add a database download, ensure quotes are uniq.
    Apply RuboCop recommendations.
    
    Bump version.

--- ssqdb/ssqdb.rb
+++ ssqdb/ssqdb.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
 # ssqdb.rb
 # Author: William Woodruff
@@ -20,80 +21,81 @@ require "erb"
 require "cgi"
 require "json"
 
-VERSION = 3
+VERSION = 4
 
 HEADER = <<~EOS
-<html>
-  <head>
-    <meta charset="UTF-8">
-    <title>ssqdb <%= " - quote \\#\#{index}" if defined? index %></title>
-    <style type="text/css">
-      body {
-        background-color: #F6F6F6;
-        font-family: monospace;
-        color: #222;
-        max-width: 50em;
-        margin: auto;
-        padding: 1em;
-      }
-
-      a {
-        color: #555;
-      }
-
-      pre {
-        overflow: auto;
-        white-space: pre-wrap;
-      }
-
-      .quote-link {
-        text-decoration: none;
-      }
-
-      .quote-header {
-        padding-left: 0.5em;
-        padding-top: 0.5em;
-        color: #555;
-      }
-
-      .quote-text {
-        padding: 0 .5em .5em .5em;
-      }
-
-      .quote {
-        padding: 0;
-        margin: 0;
-      }
-
-      .quote:nth-of-type(odd) {
-        background-color: #e0e0e0;
-        padding-bottom: 0.1em;
-        margin-bottom: -0.5em;
-      }
-    </style>
-    <script type="text/javascript">
-      function makeUrl(count) {
-        var num = Math.floor(Math.random() * count);
-        window.location = "quote" + num + ".html";
-      }
-
-      function randomQuote() {
-        var req = new XMLHttpRequest();
-        req.onreadystatechange = function() {
-          if (req.readyState == 4 && req.status == 200) {
-            makeUrl(req.responseText);
-          }
+  <html>
+    <head>
+      <meta charset="UTF-8">
+      <title>ssqdb <%= " - quote \\#\#{index}" if defined? index %></title>
+      <style type="text/css">
+        body {
+          background-color: #F6F6F6;
+          font-family: monospace;
+          color: #222;
+          max-width: 50em;
+          margin: auto;
+          padding: 1em;
         }
 
-        req.open("GET", "count", true);
-        req.send(null);
-      }
-    </script>
-  </head>
-  <body>
-    <a href="index.html">all</a> /
-    <a href="#" id="random" onclick="randomQuote();">random</a> /
-    <a href="about.html">about</a>
+        a {
+          color: #555;
+        }
+
+        pre {
+          overflow: auto;
+          white-space: pre-wrap;
+        }
+
+        .quote-link {
+          text-decoration: none;
+        }
+
+        .quote-header {
+          padding-left: 0.5em;
+          padding-top: 0.5em;
+          color: #555;
+        }
+
+        .quote-text {
+          padding: 0 .5em .5em .5em;
+        }
+
+        .quote {
+          padding: 0;
+          margin: 0;
+        }
+
+        .quote:nth-of-type(odd) {
+          background-color: #e0e0e0;
+          padding-bottom: 0.1em;
+          margin-bottom: -0.5em;
+        }
+      </style>
+      <script type="text/javascript">
+        function makeUrl(count) {
+          var num = Math.floor(Math.random() * count);
+          window.location = "quote" + num + ".html";
+        }
+
+        function randomQuote() {
+          var req = new XMLHttpRequest();
+          req.onreadystatechange = function() {
+            if (req.readyState == 4 && req.status == 200) {
+              makeUrl(req.responseText);
+            }
+          }
+
+          req.open("GET", "count", true);
+          req.send(null);
+        }
+      </script>
+    </head>
+    <body>
+      <a href="index.html">all</a> /
+      <a href="#" id="random" onclick="randomQuote();">random</a> /
+      <a href="quotes.db">db</a> /
+      <a href="about.html">about</a>
 EOS
 
 FOOTER = <<~EOS
@@ -106,6 +108,9 @@ QUOTE = <<~EOS
   <h5 class="quote-header">
     quote #<%= index %>
     <a class="quote-link" href="quote<%= index %>.html">(&rarr;)</a>
+    <% if opts[:json] %>
+      <a class="quote-link" href="quote<%= index %>.json">(json)</a>
+    <% end %>
   </h5>
   <pre class="quote-text">
   <%= quote %>
@@ -191,7 +196,7 @@ abort("That isn't a file.") unless File.file?(quotes_file)
 abort("Output directory doesn't exist.") unless Dir.exist?(output_dir)
 
 quotes_string = File.read(quotes_file)
-quotes = quotes_string.split(/^%$/).map(&:strip).reject(&:empty?)
+quotes = quotes_string.split(/^%$/).map(&:strip).reject(&:empty?).uniq
 
 abort("This file doesn't look like a quote DB.") if quotes.empty?
 
@@ -200,6 +205,7 @@ html_quotes = quotes.map do |quote|
 end
 
 File.write(File.join(output_dir, "count"), quotes.size)
+File.write(File.join(output_dir, "quotes.db"), quotes_string)
 File.write(File.join(output_dir, "index.html"), ERB.new(INDEX_PAGE).result(binding))
 File.write(File.join(output_dir, "about.html"), ERB.new(ABOUT_PAGE).result(binding))
 
